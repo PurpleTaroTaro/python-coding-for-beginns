f42af9b88eba76a594d41375694c1101d4343771
taro <taro.haniko@gmail.com>
2026-03-29 22:47:55 -0400

    Remove greeting and friend iteration code

--- python.py
+++ python.py
@@ -1,9 +1,1 @@
-name=input('what is your name?')
-print(f'hi,{name}.')
-print("my name is python")
-def greet(name):
-    print('hello',name)
-    greet('jack')
-    friends=['haru chan', 'haniko', 'zato', 'miku',]
-    for i, name in enumerate(friends):
-        print(f"iteration{i} is {name}")
+
